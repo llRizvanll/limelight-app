80738049a3f292320f3bbf02405a7e044d6323ac
llRizvanll <torizvan@gmail.com>
2024-12-17 13:56:22 +0530

    whatsanumber changed function

--- src/components/Buttons/ChatButton/index.tsx
+++ src/components/Buttons/ChatButton/index.tsx
@@ -1,31 +1,41 @@
-'use client';
+"use client";
 
-import styles from './ChatButton.module.css';
+import styles from "./ChatButton.module.css";
 
 interface IProps {
   color: string;
   bgColor?: string;
-  hoverolor?:string;
-  number?:string;
+  hoverolor?: string;
+  number?: string;
 }
 
-const ChatButton = ({ color, bgColor, hoverolor , number }: IProps) => {
+const ChatButton = ({ color, bgColor, hoverolor, number }: IProps) => {
   const whatsappNumber = number;
 
   const whatsappLink = `https://wa.me/${whatsappNumber}`;
 
+  const sendMessage = () => {
+    let url = 'whatsapp://send?text=Hello&phone='+{whatsappNumber};
+    window.open(url, '_blank'); 
+  };
+
+  
+
   return (
     <div className={styles.chatButtonContainer}>
-      <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+      {/* <a href={whatsappLink} target="_blank" rel="noopener noreferrer"> */}
         <button
-          className={`${styles.chatButton} ${color} ${bgColor || 'bg-custom-blue'} ${hoverolor ||  'hover:bg-black'} ${hoverolor  ? 'p-0' : 'p-[14px]'}`}
+          className={`${styles.chatButton} ${color} ${
+            bgColor || "bg-custom-blue"
+          } ${hoverolor || "hover:bg-black"} ${hoverolor ? "p-0" : "p-[14px]"}`}
           style={{
             backgroundColor: bgColor, // Inline style for bgColor if needed
           }}
+          onClick={sendMessage}
         >
           Chat on WhatsApp
         </button>
-      </a>
+      {/* </a> */}
     </div>
   );
 };
